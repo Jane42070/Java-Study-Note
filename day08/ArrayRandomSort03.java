51f2dfa4b5a93e8a714de3e061c6adba4f980575
Qilin Jane <1377219787@qq.com>
2022-07-23 17:54:10 +0800

    day08 提交

--- day08/ArrayRandomSort03.java
+++ day08/ArrayRandomSort03.java
@@ -10,10 +10,14 @@ import java.util.Random;
 public class ArrayRandomSort03 {
 	public static void main(String[] args) {
 		int arr[] = { 12, 45, 98, 73, 60 };
+		Random rand = new Random();
 		for (int i = 0; i < arr.length; i++) {
-			Random rand = new Random();
 			int index = rand.nextInt(arr.length);
+			int temp = arr[i];
 			arr[i] = arr[index];
+			arr[index] = temp;
+		}
+		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
 	}
